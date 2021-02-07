7ad2f24304d28d1d3cd4f7f7c2f9fe388fc28e6d
Hari Palempati <palempatihari@gmail.com>
2021-02-07 11:53:25 -0800

    [Hari] - Updated UI and added keyboard dismiss support once clicked on continue button..

--- HPGitCommits/UI/HPGitHomeViewController.swift
+++ HPGitCommits/UI/HPGitHomeViewController.swift
@@ -8,15 +8,16 @@
 import UIKit
 
 class HPGitHomeViewController: UIViewController {
-
     @IBOutlet weak var ownerNameTxtField: UITextField!
     @IBOutlet weak var repositoryNameTxtField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func navigateToCommitsViewController(_ sender: Any) {
+        self.view.endEditing(true)
     }
     
 }
