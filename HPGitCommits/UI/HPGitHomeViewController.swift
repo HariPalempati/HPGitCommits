808cfcb42dcac9e6dd82ee07f0bf7e9e43c603de
Hari Palempati <palempatihari@gmail.com>
2021-02-07 21:09:42 -0800

    [Hari] - Added error alert to be displayed when something went wrong.

--- HPGitCommits/UI/HPGitHomeViewController.swift
+++ HPGitCommits/UI/HPGitHomeViewController.swift
@@ -24,16 +24,41 @@ class HPGitHomeViewController: UIViewController {
         let viewModel = HPGitHomeViewModel(ownerName: ownerNameValue, repositoryName: repositoryNameValue)
         if viewModel.isValidOwnerName(), viewModel.isValidRepositoryName() {
             viewModel.makeGitCommitsService { (response, error) in
-                if let responseValue = response {
+                if let errorValue = error {
+                    DispatchQueue.main.async {
+                        self.displayErrorAlert(message: errorValue.localizedDescription)
+                    }
+                } else if let responseValue = response {
                     DispatchQueue.main.async {
                         let commitsViewController = HPGitCommitsViewController()
                         let commitsViewModel = HPGitCommitsViewModel(commitsData: responseValue)
                         commitsViewController.viewModel = commitsViewModel
                         self.navigationController?.pushViewController(commitsViewController, animated: true)
                     }
+                } else {
+                    DispatchQueue.main.async {
+                        self.displayErrorAlert(message: nil)
+                    }
                 }
             }
+        } else {
+            DispatchQueue.main.async {
+                self.displayErrorAlert(message: "Please enter Owner and Repository names and shouldn't contain any spaces.")
+            }
         }
+        ownerNameTxtField.text = nil
+        repositoryNameTxtField.text = nil
+    }
+}
+
+extension HPGitHomeViewController {
+    func displayErrorAlert(message: String?) {
+        let alertViewController = UIAlertController(title: "HPGitCommits", message: message ?? "Please enter a public Owner and Repository name and try again.", preferredStyle: .alert)
+        let dismissAction = UIAlertAction(title: "OK", style: .default) { (_) in
+            alertViewController.dismiss(animated: true, completion: nil)
+        }
+        alertViewController.addAction(dismissAction)
+        present(alertViewController, animated: true, completion: nil)
     }
 }
 
